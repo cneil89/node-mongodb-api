34fde78483dcd6498cff4eaef1fb4c0de977385b
cneil89 <ceneil@oakland.edu>
2019-01-23 00:37:15 -0500

    surpress deprication warinings

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true});
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+});
 
 module.exports = {
     mongoose
