94d914b342e921e514d7d3bdcaa6de21e43881c7
cneil89 <ceneil@oakland.edu>
2019-01-23 00:17:21 -0500

    Associate users with todos

--- server/tests/seed/seed.js
+++ server/tests/seed/seed.js
@@ -17,17 +17,23 @@ const users = [{
 }, {
    _id: userTwoId,
    email: 'Jen@example.com',
-   password: 'test456'
+   password: 'test456',
+   tokens: [{
+      access: 'auth',
+      token: jwt.sign({_id: userTwoId, access: 'auth'}, 'abc123').toString()
+    }]
 }];
 
 const todos = [{
    _id: new ObjectID(),
-    text: 'first test todo'
+   text: 'first test todo',
+   _creator: userOneId
 }, {
    _id: new ObjectID(),
    text: 'second test todo',
    completed: true,
-   completedAt: 333
+   completedAt: 333,
+   _creator: userTwoId
 }];
 
 const populateTodos = (done) => {
